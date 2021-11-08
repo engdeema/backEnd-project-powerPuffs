1451b9602651d9ac4e7af3e25345682466e663f5
Deema Burhama <eng.dburhama@gmail.com>
2021-11-08 18:21:28 +0300

    checking fetc catego

--- apis/categories/categories.routes.js
+++ apis/categories/categories.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { fetchcategory } = require("./categories.controllers");
 
 // Create a mini express application
 const router = express.Router();
+
+router.get("/", fetchcategory);
 module.exports = router;
