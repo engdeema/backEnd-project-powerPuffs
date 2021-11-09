07af6c30dca5059269e27929bc85f22a8647c303
Deema Burhama <eng.dburhama@gmail.com>
2021-11-09 13:54:26 +0300

    done with create category

--- apis/categories/categories.controllers.js
+++ apis/categories/categories.controllers.js
@@ -1,9 +1,29 @@
 const Categories = require("../../db/models/Categories");
 
-exports.fetchcategory = async (req, res, next) => {
+exports.fetchCategories = async (req, res, next) => {
   try {
     const category = await Categories.find();
     return res.status(200).json(category);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// exports.categoriesListFetch = async (req, res, next) => {
+//   try {
+//     //هذا اللي عليه البوبيوليت انقريدينت ،، داخل المودل كاتيجوري
+//     const categories = await Category.find().populate("recipes");
+//     return res.json(categories);
+//     console.log(categories);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+exports.createCategories = async (req, res, next) => {
+  try {
+    const newCategory = await Categories.create(req.body);
+    return res.status(201).json(newCategory);
   } catch (error) {
     next(error);
   }
--- apis/ingredients/ingredients.controllers.js
+++ apis/ingredients/ingredients.controllers.js
@@ -1,1 +1,19 @@
-const Ingredients = require("../../db/models/Ingredients");
+// const Ingredients = require("../../db/models/Ingredients");
+
+// exports.fetchingredients = async (req, res, next) => {
+//   try {
+//     const ingredients = await Ingredients.find();
+//     return res.status(200).json(ingredients);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+// exports.ingredientsListFetch = async (req, res, next) => {
+//   try {
+//     //populate :نفس سبلنق المودل نبي نعبيه
+//     const ingredients = await Categories.find().populate("categories");
+//     return res.json(ingredients);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
