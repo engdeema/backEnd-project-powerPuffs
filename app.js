3515eb25d8e0a51da3c531aa76b1f2ec4c21e5a3
Kitty <zainab_97@hotmail.co.uk>
2021-11-09 19:24:32 +0300

    logger done

--- app.js
+++ app.js
@@ -11,25 +11,28 @@ const userRoutes = require("./apis/users/users.routes");
 const passport = require("passport");
 const { localStrategy, jwtStrategy } = require("./middleware/passport");
 const errorHandler = require("./middleware/errorHandler");
+const logger = require("./middleware/logger");
 
 const connectDB = require("./db/database");
+
 // calling should be before "listen" function
 connectDB();
+app.use(express.json());
+app.use(cors());
+app.use(errorHandler);
+app.use(logger);
 
 app.use(passport.initialize());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
+// routes
+app.use("/api/CategoryList", categoriesRoutes);
+// app.use("/?", ingredientsRoutes);
+// app.use("/?", recipesRoutes);
+app.use("/api", userRoutes);
+
 //bind the app with port 8000
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Application running on localhost:${PORT}`));
 // to have the data passed from body when req.
-app.use(express.json());
-app.use(cors());
-app.use(errorHandler);
-
-// routes
-app.use("/api/categories", categoriesRoutes);
-// app.use("/?", ingredientsRoutes);
-// app.use("/?", recipesRoutes);
-app.use("/api", userRoutes);
