069fe28740a60816ee93b23ef972728cccb6b0cf
Deema Burhama <eng.dburhama@gmail.com>
2021-11-10 11:06:30 +0300

    done category recipe

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
+const mongooseSlugPlugin = require("mongoose-slug-plugin");
 
-const RecipesSchema = new mongoose.Schema({
-  name: String,
-  //   // slug: String,
-  image: { type: String },
+const RecipesSchema = mongoose.Schema(
+  {
+    name: String,
+    slug: String,
+    image: { type: String },
 
-  categories: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
+    categories: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    // ingredients: [
+    //   {
+    //     // to link them
+    //     type: Schema.Types.ObjectId,
+    //     // اسم المودل
+    //     ref: "Ingredients",
+    //   },
+    // ],
   },
-  // ingredients: [
-  //   {
-  //     // to link them
-  //     type: Schema.Types.ObjectId,
-  //     // اسم المودل
-  //     ref: "Ingredients",
-  //   },
-  // ],
-});
+  { timestamps: true }
+);
+RecipesSchema.plugin(mongooseSlugPlugin, { tmpl: "<%=name%>" });
+
 module.exports = mongoose.model("Recipes", RecipesSchema);
